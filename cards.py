f90bf62951e2ae4ae4b8728bc8bf2426f58d4b2d
Dragos Velicanu <velicanu@mit.edu>
2017-10-18 07:05:20 -0400

    cards problem

---
+++ cards.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = []
+for i in range(0,52) :
+    x.append((-1)**i)
+amax = []
+for j in range(0,1000) :
+    np.random.shuffle(x)
+    max = 0
+    sum = 0
+    for i in x :
+        sum+=i
+        if(sum>max) :
+            max = sum
+    amax.append(max)
+
+print(np.mean(amax))
+# myhist = plt.hist(amax,10,(0,10))
+# plt.show()
+            
